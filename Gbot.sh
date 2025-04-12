7f37155e41c9e11388eebf5b216899ed62139ad8
heerabete <125117306+heerabete@users.noreply.github.com>
2025-04-12 16:17:37 +0530

    Create Gbot.sh

---
+++ Gbot.sh
@@ -0,0 +1,11 @@
+##BOT V1
+GAIA_DOMAIN=0x3xxxx
+BASE_URL=gaia.domains
+GAIANET_AUTH_TOKEN=gaia-MThkZjJlMjMtMGFmMS00ZjI1LWJkNWItZjQ4NzIwZDk1NDhj--kH1ubAx7KaaV3_d
+
+GROQ_API_KEY=gsk_xxxxxxx
+
+##BOT V2
+API_URL=https://legend.gaia.domains/v1/chat/completions #FOR EXAMPLE i join domain to bybit.gaia.domains. DONT DELET THIS [/v1/chat/completions]
+NUM_THREADS=10
+SLEEP_INTERVAL=5
